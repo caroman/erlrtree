c928f91e31af0f03a367b71989435f0ed4995d9d
Carlos Roman <caroman@airsage.com>
2013-04-12 06:46:10 -0400

    Add dependencies erlgeom and erlogr

--- apps/rtree/src/rtree.erl
+++ apps/rtree/src/rtree.erl
@@ -7,7 +7,9 @@
 %%%%%
 %% @spec intersects(string(), string()) -> none()
 %
-intersects(Latitude, Longitude) -> 
+intersects(Latitude, Longitude) ->
+    Pt = {'Point',[0.0, 1.1]},
+    Pt1 = erlgeom:to_geom(Pt),
     io:format("Intersects: ~p~n", [{Latitude, Longitude}]),
     {ok, true}.
 
